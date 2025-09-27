fc29bb149260fad0f1966dfbf47dfb098d346674
Marichichu <marianellisha.miranda@benilde.edu.ph>
2025-09-27 10:06:17 +0800

    Added Module 0

---
+++ Module 0/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("HelloW World!"));
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("What is  your Name?");
+    String name = scanner.nextLine();
+
+    System.out.println("Hello " + name);
+
+}
+
